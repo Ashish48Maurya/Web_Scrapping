24a433ede3fa9a9967198c034f2a309bed9651be
Ashish48Maurya <maurya.48.ashish@gmail.com>
2025-02-02 20:06:09 +0530

    added CORS middleware

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import * as cheerio from "cheerio";
 import express from "express";
+import cors from "cors"
 const app = express();
 const port = process.env.PORT || 8000;
 
+
+app.use(cors())
 app.use(express.json());
 
 async function findID(url) {

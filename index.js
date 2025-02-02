d923a36e89b9b7758ac22bdc93e99ff2ed909244
Ashish48Maurya <maurya.48.ashish@gmail.com>
2025-02-02 15:29:37 +0530

    implemented web scraping functionality using Cheerio to fetch CVE details and product vulnerabilities

--- index.js
+++ index.js
@@ -6,126 +6,151 @@ const app = express();
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-    const { tool, version } = req.body;
-    if (!tool || !version) {
-        return res.status(400).json({ message: "Tool and version are required" });
+    const { tool } = req.body;
+    if (!tool) {
+        return res.status(400).json({ message: "Software aname is required" });
     }
 
     try {
-        const response = await fetch(`https://cve.mitre.org/cgi-bin/cvekey.cgi?keyword=${tool}`);
+        const response = await fetch(`https://www.cvedetails.com/product-search.php?vendor_id=0&search=${tool}`);
         const body = await response.text();
         const $ = cheerio.load(body);
 
-        const tableRows = $("#TableWithRules tbody tr");
-        let cveIds = [];
+        const tableRows = $("#contentdiv > div > main > div.table-responsive > table > tbody > tr");
 
-        tableRows.each((_, row) => {
-            const cveId = $(row).find("td").first().text().trim();
-            const cveDesc = $(row).find("td").eq(1).text().trim();
-            if (cveId.startsWith("CVE-")) {
-                cveIds.push({ cveId, cveDesc });
+        const data = [];
+
+        tableRows.each((index, row) => {
+            const columns = $(row).find("td");
+            if (columns.length >= 6) {
+                const productColumn = $(columns[1]).find("a");
+        
+                data.push({
+                    serial_no: $(columns[0]).text().trim(),
+                    product_name: productColumn.text().trim(), // Extract product name
+                    product_link: `https://www.cvedetails.com${productColumn.attr("href")}`,
+                    vendor_name: $(columns[2]).text().trim(),
+                    no_of_vuln: $(columns[3]).text().trim(),
+                    prd_type: $(columns[4]).text().trim(),
+                });
             }
         });
 
-        const cveDetails = [];
+        //go to all the product link and in that page extract url of a from $('#pagingt > a)
 
-        for (const cveId of cveIds) {
-            try {
-                const ans = await fetch(`https://cveawg.mitre.org/api/cve/${cveId.cveId}`);
-                const res = await ans.json();
-                if (res && res.containers && res.containers.cna && res.containers.cna.metrics && res.containers.cna.metrics.length > 0) {
-                    const metric = res.containers.cna.metrics[0];
-                    if (metric?.cvssV3_1) {
-                        if (metric?.cvssV3_1.version == version) {
-                            console.log("push");
-                            cveDetails.push({
-                                date_published: res.cveMetadata.datePublished,
-                                cveId: cveId.cveId,
-                                cveDesc: cveId.cveDesc,
-                                score: metric ? metric.cvssV3_1.baseScore : null,
-                                severity: metric ? metric.cvssV3_1.baseSeverity : null,
-                                version: metric ? metric.cvssV3_1.version : null,
-                                vectorString: metric ? metric.cvssV3_1.vectorString : null
-                            });
-                        }
-                    }
-                    else if (metric?.cvssV4_0) {
-                        if (metric?.cvssV4_0.version === version) {
-                            cveDetails.push({
-                                date_published: res.cveMetadata.datePublished,
-                                cveId: cveId.cveId,
-                                cveDesc: cveId.cveDesc,
-                                score: metric ? metric.cvssV4_0.baseScore : null,
-                                severity: metric ? metric.cvssV4_0.baseSeverity : null,
-                                version: metric ? metric.cvssV4_0.version : null,
-                                vectorString: metric ? metric.cvssV4_0.vectorString : null
-                            });
-                        }
-                    }
-                    else if (metric?.cvssV3_0) {
-                        if (metric?.cvssV3_0.version === version) {
-                            cveDetails.push({
-                                date_published: res.cveMetadata.datePublished,
-                                cveId: cveId.cveId,
-                                cveDesc: cveId.cveDesc,
-                                score: metric ? metric.cvssV3_0.baseScore : null,
-                                severity: metric ? metric.cvssV3_0.baseSeverity : null,
-                                version: metric ? metric.cvssV3_0.version : null,
-                                vectorString: metric ? metric.cvssV3_0.vectorString : null
-                            });
-                        }
-                    }
-                    else if (metric?.cvssV2_0) {
-                        if (metric?.cvssV2_0.version === version) {
-                            cveDetails.push({
-                                date_published: res.cveMetadata.datePublished,
-                                cveId: cveId.cveId,
-                                cveDesc: cveId.cveDesc,
-                                score: metric ? metric.cvssV2_0.baseScore : null,
-                                severity: metric ? metric.cvssV2_0.baseSeverity : null,
-                                version: metric ? metric.cvssV2_0.version : null,
-                                vectorString: metric ? metric.cvssV2_0.vectorString : null
-                            });
-                        }
-                    }
-                    else {
-                        cveDetails.push({
-                            cveId: cveId.cveId,
-                            cveDesc: cveId.cveDesc,
-                            score: null,
-                            severity: null,
-                            version: null,
-                            vectorString: null
-                        });
-                    }
-                }
-                else {
-                    cveDetails.push({
-                        cveId: cveId.cveId,
-                        cveDesc: cveId.cveDesc,
-                        score: null,
-                        severity: null,
-                        version: null,
-                        vectorString: null
-                    });
-                }
-            } catch (error) {
-                console.error(`Error fetching details for ${cveId.cveId}:`, error.message);
-                cveDetails.push({
-                    cveId: cveId.cveId,
-                    cveDesc: cveId.cveDesc,
-                    score: null,
-                    severity: null,
-                    version: null,
-                    vectorString: null
-                });
-            }
-        }
 
-        if (cveDetails.length === 0) {
-            return res.status(404).json({ message: `No data found for ${tool} version ${version}` });
-        }
-        return res.status(200).json(cveDetails);
+
+        // let cveIds = [];
+
+        // tableRows.each((_, row) => {
+        //     const cveId = $(row).find("td").first().text().trim();
+        //     const cveDesc = $(row).find("td").eq(1).text().trim();
+        //     if (cveId.startsWith("CVE-")) {
+        //         cveIds.push({ cveId, cveDesc });
+        //     }
+        // });
+
+        return res.json('done');
+
+        // const cveDetails = [];
+
+        // for (const cveId of cveIds) {
+        //     try {
+        //         const ans = await fetch(`https://cveawg.mitre.org/api/cve/${cveId.cveId}`);
+        //         const res = await ans.json();
+        //         if (res && res.containers && res.containers.cna && res.containers.cna.metrics && res.containers.cna.metrics.length > 0) {
+        //             const metric = res.containers.cna.metrics[0];
+        //             if (metric?.cvssV3_1) {
+        //                 if (metric?.cvssV3_1.version == version) {
+        //                     console.log("push");
+        //                     cveDetails.push({
+        //                         date_published: res.cveMetadata.datePublished,
+        //                         cveId: cveId.cveId,
+        //                         cveDesc: cveId.cveDesc,
+        //                         score: metric ? metric.cvssV3_1.baseScore : null,
+        //                         severity: metric ? metric.cvssV3_1.baseSeverity : null,
+        //                         version: metric ? metric.cvssV3_1.version : null,
+        //                         vectorString: metric ? metric.cvssV3_1.vectorString : null
+        //                     });
+        //                 }
+        //             }
+        //             else if (metric?.cvssV4_0) {
+        //                 if (metric?.cvssV4_0.version === version) {
+        //                     cveDetails.push({
+        //                         date_published: res.cveMetadata.datePublished,
+        //                         cveId: cveId.cveId,
+        //                         cveDesc: cveId.cveDesc,
+        //                         score: metric ? metric.cvssV4_0.baseScore : null,
+        //                         severity: metric ? metric.cvssV4_0.baseSeverity : null,
+        //                         version: metric ? metric.cvssV4_0.version : null,
+        //                         vectorString: metric ? metric.cvssV4_0.vectorString : null
+        //                     });
+        //                 }
+        //             }
+        //             else if (metric?.cvssV3_0) {
+        //                 if (metric?.cvssV3_0.version === version) {
+        //                     cveDetails.push({
+        //                         date_published: res.cveMetadata.datePublished,
+        //                         cveId: cveId.cveId,
+        //                         cveDesc: cveId.cveDesc,
+        //                         score: metric ? metric.cvssV3_0.baseScore : null,
+        //                         severity: metric ? metric.cvssV3_0.baseSeverity : null,
+        //                         version: metric ? metric.cvssV3_0.version : null,
+        //                         vectorString: metric ? metric.cvssV3_0.vectorString : null
+        //                     });
+        //                 }
+        //             }
+        //             else if (metric?.cvssV2_0) {
+        //                 if (metric?.cvssV2_0.version === version) {
+        //                     cveDetails.push({
+        //                         date_published: res.cveMetadata.datePublished,
+        //                         cveId: cveId.cveId,
+        //                         cveDesc: cveId.cveDesc,
+        //                         score: metric ? metric.cvssV2_0.baseScore : null,
+        //                         severity: metric ? metric.cvssV2_0.baseSeverity : null,
+        //                         version: metric ? metric.cvssV2_0.version : null,
+        //                         vectorString: metric ? metric.cvssV2_0.vectorString : null
+        //                     });
+        //                 }
+        //             }
+        //             else {
+        //                 cveDetails.push({
+        //                     cveId: cveId.cveId,
+        //                     cveDesc: cveId.cveDesc,
+        //                     score: null,
+        //                     severity: null,
+        //                     version: null,
+        //                     vectorString: null
+        //                 });
+        //             }
+        //         }
+        //         else {
+        //             cveDetails.push({
+        //                 cveId: cveId.cveId,
+        //                 cveDesc: cveId.cveDesc,
+        //                 score: null,
+        //                 severity: null,
+        //                 version: null,
+        //                 vectorString: null
+        //             });
+        //         }
+        //     } catch (error) {
+        //         console.error(`Error fetching details for ${cveId.cveId}:`, error.message);
+        //         cveDetails.push({
+        //             cveId: cveId.cveId,
+        //             cveDesc: cveId.cveDesc,
+        //             score: null,
+        //             severity: null,
+        //             version: null,
+        //             vectorString: null
+        //         });
+        //     }
+        // }
+
+        // if (cveDetails.length === 0) {
+        //     return res.status(404).json({ message: `No data found for ${tool} version ${version}` });
+        // }
+        // return res.status(200).json(cveDetails);
 
     } catch (error) {
         console.error("Error:", error);
--- index1.js
+++ index1.js
@@ -1,37 +1,116 @@
+import * as cheerio from "cheerio";
 
+async function findID(url) {
+        const response = await fetch(url);
+        const body = await response.text();
+        const $ = cheerio.load(body);
 
-// const cveData = await Promise.all(
-        //     cveIds.map(async (cveId) => {
-        //         const browser = await puppeteer.launch();
-        //         const page = await browser.newPage();
-        //         await page.goto(`https://www.cve.org/CVERecord?id=${cveId}`, { waitUntil: 'domcontentloaded' });
+        const tableRows = $("#searchresults > div > div");
+        const data = [];
 
-        //         const pageExist = await page
-        //             .waitForSelector('#cvss-table > table')
-        //             .then(() => true)
-        //             .catch(() => false);
+        tableRows.each((index, row) => {
+                const childDivs = $(row).children("div");
 
-        //         if (!pageExist) {
-        //             await browser.close();
-        //             return { cveId, message: 'Page not found' };
+                if (childDivs.length >= 2) {
+                        const firstDivChildren = $(childDivs[0]).children("div");
+                        const firstDivData = firstDivChildren.map((i, el) => $(el).text().trim()).get();
+
+                        const secondDivChildren = $(childDivs[1]).children("div");
+
+                        let secondDivData = [];
+                        secondDivChildren.each((i, el) => {
+                                const innerDivs = $(el).children("div");
+
+                                if (innerDivs.length >= 2) {
+                                        const nestedKey = $(innerDivs[0]).text().trim();
+
+                                        let nestedValue;
+                                        if (i === 0) {
+                                                nestedValue = $(innerDivs[1]).children("div").text().trim();
+                                        }
+                                        else if (i === 1) {
+                                                nestedValue = $(innerDivs[1]).children("span").text().trim();
+                                        }
+                                        else {
+                                                nestedValue = $(innerDivs[1]).text().trim();
+                                        }
+                                        secondDivData.push({ key: nestedKey, value: nestedValue });
+                                }
+                                else {
+                                        secondDivData.push({ key: $(innerDivs[0]).text().trim(), value: $(el).text().trim() });
+                                }
+                        });
+
+                        data.push({
+                                firstDiv: firstDivData,
+                                secondDiv: secondDivData
+                        });
+                }
+        });
+        return data;
+}
+
+async function fetchData(tool) {
+        const response = await fetch(`https://www.cvedetails.com/product-search.php?vendor_id=0&search=${tool}`);
+        const body = await response.text();
+        const $ = cheerio.load(body);
+
+        const tableRows = $("#contentdiv > div > main > div.table-responsive > table > tbody > tr");
+
+        const data = [];
+
+        for (let row of tableRows) {
+                const columns = $(row).find("td");
+                if (columns.length >= 6) {
+                        const productColumn = $(columns[1]).find("a");
+                        const vulnColumn = $(columns[3]).find("a");
+                        const no_of_vuln = $(columns[3]).text().trim();
+
+                        const productLink = `https://www.cvedetails.com${productColumn.attr("href")}`;
+                        const vulnLink = `https://www.cvedetails.com${vulnColumn.attr("href")}`;
+
+                        const data1 = [];
+
+                        if (no_of_vuln > 0) {
+                                const details = await findID(vulnLink);
+                                const sample = [];
+                                for (let i = 0; i < details.length; i++) {
+                                        const currentDetail = details[i];
+                                        const firstDivData = currentDetail.firstDiv;
+                                        const secondDivData = currentDetail.secondDiv;
+
+                                        const combinedData = {
+                                                firstDiv: firstDivData,
+                                                secondDiv: secondDivData
+                                        };
+                                        sample.push(combinedData);
+                                }
+                                data1.push(sample);
+                        }
+                        data.push({
+                                serial_no: $(columns[0]).text().trim(),
+                                product_name: productColumn.text().trim(),
+                                product_link: productLink,
+                                vendor_name: $(columns[2]).text().trim(),
+                                no_of_vuln: $(columns[3]).text().trim(),
+                                vuln_link: vulnLink,
+                                prd_type: $(columns[4]).text().trim(),
+                                details: data1
+                        });
+                }
+        }
+        return data;
+}
+
+const res = await fetchData("linux");
+res.forEach((element) => {
+        console.log(element);
+        // if (element.details.length > 0) {
+        //         console.log(element.details[0].length);
+        //         for (let i = 0; i < element.details[0].length; i++) {
+        //                 console.log(element.details[0][i].firstDiv);
+        //                 console.log(element.details[0][i].secondDiv);
+        //                 console.log("---------------------------------------------------");
         //         }
-
-        //         const content = await page.evaluate(() => {
-        //             const rows = document.querySelectorAll('#cvss-table > table > tbody > tr');
-        //             return Array.from(rows).map(row => {
-        //                 const cols = row.querySelectorAll('td');
-        //                 return {
-        //                     score: cols[0] ? cols[0].innerText : null,
-        //                     severity: cols[1] ? cols[1].innerText : null,
-        //                     version: cols[2] ? cols[2].innerText : null,
-        //                     vectorString: cols[3] ? cols[3].innerText : null
-        //                 };
-        //             });
-        //         });
-
-        //         await browser.close();
-        //         return { cveId, content };
-        //     })
-        // );
-
-        // console.log("Res:", JSON.stringify(cveData, null, 2));
+        // }
+});
